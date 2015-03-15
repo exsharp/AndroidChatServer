c141c73e669f0d6c06ff2e90de681caf261a9ed0
exsharp <zfliu_6@163.com>
2015-03-15 12:04:11 +0800

    friendlist

--- server.go
+++ server.go
@@ -48,10 +48,10 @@ func Server(conn net.Conn) {
 		}
 		fmt.Println(sour)
 		switch sour.TYPE {
-		case "LOGIN":
-			loginHandler(conn, sour)
 		case "REGISTER":
 			registerHandler(conn, sour)
+		case "LOGIN":
+			loginHandler(conn, sour)
 		case "MSG":
 			msgHandler(conn, sour)
 		case "ADDFRIEND":
@@ -71,55 +71,32 @@ func Server(conn net.Conn) {
 func registerHandler(conn net.Conn, sour JSON) {
 	var send JSON
 	send.TYPE = "REGISTER"
-	acount := sour.CONTENT[0]
+	account := sour.CONTENT[0]
 	password := sour.CONTENT[1]
 	email := sour.CONTENT[2]
 
-	db, _ := sql.Open("mysql", "root:root@/android?chatset=utf-8")
+	fmt.Printf("注册信息:%s,%s,%s", account, password, email)
+
+	db, err := sql.Open("mysql", "root:root@/android?charset=utf8")
+	showError(err)
 	defer db.Close()
-	rows, _ := db.Query("select uid from users where acount=?", acount)
+
+	rows, err1 := db.Query("select uid from users where acount=?", account)
+	showError(err1)
+
 	uid := 0
 	for rows.Next() {
 		rows.Scan(&uid)
 	}
+
 	if uid > 0 {
 		send.CONTENT = append(send.CONTENT, "exist")
 	} else {
-		db.Exec("insert into users(acount,password,email) values(?,?,?)", acount, password, email)
+		db.Exec("insert into users(acount,password,email) values(?,?,?)", account, password, email)
 		send.CONTENT = append(send.CONTENT, "success")
 	}
-}
-
-func msgHandler(conn net.Conn, sour JSON) {
-	var send JSON
-	t := time.Now().Format("01-02\t15:04:05")
-	to := sour.CONTENT[1]
-	send.TYPE = "MSG"
-	send.CONTENT = append(send.CONTENT, sour.CONTENT[0], sour.CONTENT[1], t, sour.CONTENT[3])
-	for i := range online {
-		if to == online[i] {
-			sendMsg(i, send)
-		}
-		fmt.Println(send)
-		//sendMsg(i, send)
-	}
+	fmt.Println(send)
 	sendMsg(conn, send)
-}
-
-func sendMsg(conn net.Conn, send JSON) {
-	END := []byte("\r\n")
-	str, _ := json.Marshal(send)
-	str = append(str, END...)
-	conn.Write(str)
-}
-
-func rcvMsg(conn net.Conn) (JSON, error) {
-	var rcv JSON
-	data := make([]byte, 1024)
-	i, err := conn.Read(data)
-	fmt.Println("客户端", conn.RemoteAddr().String(), "发来数据:", string(data[0:i]))
-	json.Unmarshal(data[0:i], &rcv)
-	return rcv, err
 }
 
 func loginHandler(conn net.Conn, sour JSON) {
@@ -138,7 +115,7 @@ func loginHandler(conn net.Conn, sour JSON) {
 	if passWord == pw && account != "" {
 		online[conn] = account
 		send.CONTENT = append(send.CONTENT, "登陆成功")
-		//friendListHandler(conn)
+		friendListHandler(conn)
 	} else {
 		if uid > 0 {
 			send.CONTENT = append(send.CONTENT, "FAIL", "没有此用户")
@@ -150,24 +127,44 @@ func loginHandler(conn net.Conn, sour JSON) {
 }
 
 func friendListHandler(conn net.Conn) {
-	//fmt.Println(online[conn] + "FLHandler")
-	db, _ := sql.Open("mysql", "root:root@/android?charset=utf8")
-	rows, _ := db.Query("select mgroup from groups where user=?", online[conn])
+
 	var send JSON
-	send.TYPE = "FRIENDGROUP"
+	send.TYPE = "FRIENDLIST"
+	send.CONTENT = append(send.CONTENT, "")
+
+	db, _ := sql.Open("mysql", "root:root@/android?charset=utf8")
+	rows, _ := db.Query("select firend,grouping from relation where account=? order by grouporder,firend", online[conn])
+	defer db.Close()
+
+	var friend string
+	var group string
+	temp := ""
 	for rows.Next() {
-		send.CONTENT = append(send.CONTENT, "")
-		mgroup := ""
-		rows.Scan(&mgroup)
-		send.CONTENT = append(send.CONTENT, mgroup)
-		rows2, _ := db.Query("select friend from friends where user=? and groups=?", online[conn], mgroup)
-		for rows2.Next() {
-			friend := ""
-			rows2.Scan(&friend)
+		temp = group
+		rows.Scan(&friend, &group)
+		if group == temp {
 			send.CONTENT = append(send.CONTENT, friend)
+		} else {
+			send.CONTENT = append(send.CONTENT, "", group, friend)
 		}
 	}
 	fmt.Println(send)
+	//sendMsg(conn, send)
+}
+
+func msgHandler(conn net.Conn, sour JSON) {
+	var send JSON
+	t := time.Now().Format("01-02\t15:04:05")
+	to := sour.CONTENT[1]
+	send.TYPE = "MSG"
+	send.CONTENT = append(send.CONTENT, sour.CONTENT[0], sour.CONTENT[1], t, sour.CONTENT[3])
+	for i := range online {
+		if to == online[i] {
+			sendMsg(i, send)
+		}
+		fmt.Println(send)
+		//sendMsg(i, send)
+	}
 	sendMsg(conn, send)
 }
 
@@ -288,4 +285,26 @@ func delgroupHandler(conn net.Conn, sour JSON) {
 	}
 	friendListHandler(conn)
 	sendMsg(conn, send)
+}
+
+func showError(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func sendMsg(conn net.Conn, send JSON) {
+	END := []byte("\r\n")
+	str, _ := json.Marshal(send)
+	str = append(str, END...)
+	conn.Write(str)
+}
+
+func rcvMsg(conn net.Conn) (JSON, error) {
+	var rcv JSON
+	data := make([]byte, 1024)
+	i, err := conn.Read(data)
+	fmt.Println("客户端", conn.RemoteAddr().String(), "发来数据:", string(data[0:i]))
+	json.Unmarshal(data[0:i], &rcv)
+	return rcv, err
 }
